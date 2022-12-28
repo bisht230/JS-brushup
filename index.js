9ba8687fc6ab504776f9c4b8d8be85a9ce2390bd
bisht230 <bishtdeepanshu007@gmail.com>
2022-12-28 15:11:22 +0530

    About objects

--- index.js
+++ index.js
@@ -25,3 +25,20 @@
 
 // console.log(flag ? name : 'Ketan')
 
+//objects
+
+const person = {
+    name : 'Deep',
+    age : 20,
+    isMarried : false
+}
+
+// const {name , age , isMarried} = person
+// console.log(name)
+// console.log(age)
+
+//role of spread operator
+
+// const person2 = {...person , name : 'Deepansh' }
+// const {name , age , isMarried} = person2;
+// console.log(name)
