472ff5f96088319b925f61ca45959e1188807667
bisht230 <bishtdeepanshu007@gmail.com>
2022-12-28 14:52:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//arrow function 
+//ternary operators
+//objects 
+//map and filters
+//async await and fetch 
+
+
+//arrow function
+
+// const demonstration = (name) => {
+//     console.log(name)
+// }
+// demonstration("Deep")
+
+//normal function 
+// function demonstration(name) {
+//   console.log(name)
+// }
+// demonstration("Deep")
+
