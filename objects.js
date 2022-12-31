6f3a487c2c07f0d2183d1a9861016d9970319b69
bisht230 <bishtdeepanshu007@gmail.com>
2022-12-31 17:32:44 +0530

    Refactoring code witjh comments

--- objects.js
+++ objects.js
@@ -1,17 +1,23 @@
-//objects
+/*
+ * objects 
+ */
 
 const person = {
-    name : 'Deep',
-    age : 20,
-    isMarried : false
-}
+  name: "Deep",
+  age: 20,
+  isMarried: false,
+};
 
-// const {name , age , isMarried} = person
-// console.log(name)
-// console.log(age)
+/*
+ * const {name , age , isMarried} = person
+ * console.log(name)
+ * console.log(age) 
+ */
 
-//role of spread operator
+/*
+ * role of spread operator 
+ */
 
-const person2 = {...person , name : 'Deepansh' }
-const {name , age , isMarried} = person2;
-console.log(name)
+const person2 = { ...person, name: "Deepansh" };
+const { name, age, isMarried } = person2;
+console.log(name);
--- spread.js
+++ spread.js
@@ -1,45 +1,42 @@
 const myVehicle = {
-    brand: 'Ford',
-    model: 'Mustang',
-    color: 'red'
-  }
-  
-  const updateMyVehicle = {
-    type: 'car',
-    year: 2021, 
-    color: 'yellow'
-  }
+  brand: "Ford",
+  model: "Mustang",
+  color: "red",
+};
 
-  const arr = {...myVehicle , ...updateMyVehicle}
-  console.log(arr)
+const updateMyVehicle = {
+  type: "car",
+  year: 2021,
+  color: "yellow",
+};
 
+const arr = { ...myVehicle, ...updateMyVehicle };
+console.log(arr);
 
-  /*
-   * In terms of array combination 
-   */
-  
-    const arr1 = [1,2,3];
-    const arr2 = [4,5,6];
-     
-    const newArr = [...arr1 , ...arr2]
-    console.log(newArr)
-    
+/*
+ * In terms of array combination
+ */
 
-    /*
-     * In terms of objects 
-     */
-     
-     const myDetails = {
-        name : "Deepanshu",
-        age : 20,
-        isMarried : false
-     }
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
 
-     const schoolDetails = {
-        name : "TPS",
-        address : "Keshav Puram"
-     }
+const newArr = [...arr1, ...arr2];
+console.log(newArr);
 
-     const final = {...myDetails , ...schoolDetails};
-     console.log(final)
-     
+/*
+ * In terms of objects
+ */
+
+const myDetails = {
+  name: "Deepanshu",
+  age: 20,
+  isMarried: false,
+};
+
+const schoolDetails = {
+  name: "TPS",
+  address: "Keshav Puram",
+};
+
+const final = { ...myDetails, ...schoolDetails };
+console.log(final);
