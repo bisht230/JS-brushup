5757e698778a1eb246bc07319292eec2f79b0151
bisht230 <bishtdeepanshu007@gmail.com>
2023-01-13 20:58:26 +0530

    adding up comment

--- calendar.js
+++ calendar.js
@@ -1,7 +1,10 @@
-function calculateDaysBetweenDates(begin, end){
+function calculateDaysBetweenDates(begin, end) {
     var beginDate = new Date(begin);
     var endDate = new Date(end);
     var days = (endDate - beginDate) / 1000 / 60 / 60 / 24;
     return days;
 }
 console.log(calculateDaysBetweenDates('2020-01-01', '2020-02-10'));
+/* 
+   ? it will return 40 
+ */
